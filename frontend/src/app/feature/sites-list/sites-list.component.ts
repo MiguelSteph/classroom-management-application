4c4368e2dfeb0fbfdba5747250f6b23a291676e6
Miguel <because_of_peace@yahoo.fr>
2021-05-24 21:29:27 +0100

    Add site list

--- frontend/src/app/feature/sites-list/sites-list.component.ts
+++ frontend/src/app/feature/sites-list/sites-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SiteService } from "../../core/services/site.service";
 
 @Component({
-  selector: 'app-sites-list',
-  templateUrl: './sites-list.component.html',
-  styleUrls: ['./sites-list.component.css']
+  selector: "app-sites-list",
+  templateUrl: "./sites-list.component.html",
+  styleUrls: ["./sites-list.component.css"],
 })
 export class SitesListComponent implements OnInit {
+  sites: Array<any> = [];
+  searchInput: string = "";
 
-  constructor() { }
+  constructor(private siteService: SiteService) {}
 
   ngOnInit(): void {
+    this.loadSites();
   }
 
+  loadSites = () => {
+    this.siteService.get().subscribe((data) => {
+      this.sites = data as Object[];
+    });
+  };
+
+  filterSites = () => {
+    if (this.searchInput.length === 0) {
+      return this.sites;
+    }
+    return this.sites.filter((site) => {
+      return (
+        site.code.toUpperCase().indexOf(this.searchInput.toUpperCase()) >= 0 ||
+        site.name.toUpperCase().indexOf(this.searchInput.toUpperCase()) >= 0
+      );
+    });
+  };
 }
