a950b3900537fd021fc4842d30676f34acda134a
Miguel <because_of_peace@yahoo.fr>
2021-06-02 02:00:05 +0100

    Add docker compose

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: "classmanagementapplication",
+  clientPwd: "h22ouDKWuNZlhpQ2",
+  authServerEndPoint: "http://authorization-server:9092",
+  resourceServerEndPoint: "http://resource-server:9090",
+  authServerDomain: "authorization-server:9092",
+  resourceServerDomain: "resource-server:9090",
+
+  pageSize: 10,
 };
